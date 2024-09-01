2940768bd666e43a431af9fb55f94c03a8e216d4
MahmoudTonsy <86135416+Mahmoud-Emad-Tonsy@users.noreply.github.com>
2024-09-01 16:26:57 +0300

    solve new problem

---
+++ edabit.com.problems/medium/secondLargest.js
@@ -0,0 +1,23 @@
+/*
+Find the Second Largest Number
+Create a function that takes an array of numbers and returns the second largest number.
+
+Examples
+secondLargest([10, 40, 30, 20, 50]) ➞ 40
+
+secondLargest([25, 143, 89, 13, 105]) ➞ 105
+
+secondLargest([54, 23, 11, 17, 10]) ➞ 23
+Notes
+There will be at least two numbers in the array.
+*/
+
+
+function secondLargest(arr) {
+	
+	let sortedArr = arr.sort((a,b)=> a - b)
+    return sortedArr[sortedArr.length - 2]
+}
+
+
+console.log(secondLargest([54, 23, 11, 17, 10])) //23
